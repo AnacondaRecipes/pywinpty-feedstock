3d5056e05ad8c8b0ce6c4b1f80932a018de3c405
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2025-12-31 13:45:53 +0100

    bypass escape character issue

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,24 @@
 @echo on
 
-REM Set environment for winpty
-set "PATH=%LIBRARY_BIN%;%PATH%"
-set "LIB=%LIBRARY_LIB%;%LIB%"
+REM Create a temp dir without spaces
+set WINPTY_TEMP=%SRC_DIR%\winpty_temp
+mkdir "%WINPTY_TEMP%"
 
-REM Create a .cargo/config.toml that sets environment for all cargo builds
-if not exist .cargo mkdir .cargo
-(
-echo [env]
-echo PATH = { value = "%PATH%", force = true }
-echo LIB = { value = "%LIB%", force = true }
-) > .cargo\config.toml
+REM Copy winpty files there
+copy "%LIBRARY_BIN%\winpty-agent.exe" "%WINPTY_TEMP%\"
+copy "%LIBRARY_BIN%\winpty.dll" "%WINPTY_TEMP%\"
+copy "%LIBRARY_LIB%\winpty.lib" "%WINPTY_TEMP%\"
+
+REM Add to PATH at the front
+set "PATH=%WINPTY_TEMP%;%PATH%"
+set "LIB=%WINPTY_TEMP%;%LIB%"
 
 REM Build
 %PYTHON% -m pip install . -vv --no-deps --no-build-isolation
 if errorlevel 1 exit 1
+
+REM Cleanup
+rmdir /s /q "%WINPTY_TEMP%"
 
 REM Generate license info
 cargo-bundle-licenses --format yaml --output "%SRC_DIR%\THIRDPARTY.yml"
