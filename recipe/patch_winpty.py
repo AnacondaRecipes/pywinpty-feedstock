639a19ea0d6d3810b875ab28c713ee72534f941f
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-01-02 14:09:47 +0100

    comment out conpty feature

--- recipe/patch_winpty.py
+++ recipe/patch_winpty.py
@@ -4,7 +4,7 @@ import time
 import glob
 
 def patch_build_rs():
-    """Find and patch winpty-rs build.rs to skip NuGet check"""
+    """Find and patch winpty-rs build.rs to disable ConPTY entirely"""
     
     max_wait = 60
     start_time = time.time()
@@ -27,11 +27,13 @@ def patch_build_rs():
                         content = f.read()
                     
                     if 'panic!("NuGet is required' in content:
-                        # Replace panic and enable conpty_local feature to allow compilation
                         new_content = content.replace(
+                            'println!("cargo:rustc-cfg=feature=\\"conpty\\"");',
+                            '// println!("cargo:rustc-cfg=feature=\\"conpty\\""); // Disabled - use only WinPTY'
+                        )
+                        new_content = new_content.replace(
                             'panic!("NuGet is required to build winpty-rs");',
-                            f'// Skip NuGet - use conda winpty and enable conpty_local for compilation\n'
-                            f'            println!("cargo:rustc-cfg=feature=\\"conpty_local\\"");\n'
+                            f'// Skip NuGet - use only conda winpty (WinPTY backend)\n'
                             f'            println!("cargo:rustc-link-search=native={lib_path}");\n'
                             f'            println!("cargo:rustc-link-lib=winpty");'
                         )
@@ -39,7 +41,7 @@ def patch_build_rs():
                         with open(filepath, 'w', encoding='utf-8') as f:
                             f.write(new_content)
                         
-                        print("Successfully patched winpty-rs build.rs!")
+                        print("Successfully patched winpty-rs build.rs - ConPTY disabled, WinPTY only!")
                         return True
         
         time.sleep(0.5)
